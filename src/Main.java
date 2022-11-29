8f1cce62728249535b8addc62a2cd0cc22745a71
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-11-29 22:08:18 +0100

    no idea why I have some errors. i literally have just copied the WORKING code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Monitor samsung = new Monitor();
+        Monitor legion = new Monitor();
+        samsung.width = 25;
+        legion.height = 144;
+
+        System.out.println(samsung.width);
+        System.out.println(legion.height);
+
+    }
+
+    class Monitor {
+        int width;
+        int height;
     }
 }
